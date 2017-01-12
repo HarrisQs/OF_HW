7ff898f99d7a08c585399c853035d90f10625a38
HarrisQs <harris32916@gmail.com>
2017-01-12 20:07:42 +0800

    Add LoseGame or WinGame

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -9,49 +9,95 @@ void ofApp::setup()
 	ofSetFrameRate(120);//給螢幕刷新速度一個目標!
 	cam.setup(1280, 960);//攝影機投出的大小
 	ofEnableAlphaBlending();//混和的 在這裡不知道混啥= =?
+	rgb.allocate(1280, 960);
+	filtered.allocate(1280, 960);
+	red.allocate(1280, 960);
+	green.allocate(1280, 960);
+	blue.allocate(1280, 960);
+	synth.load("sounds/synth.wav");
+	synth.setVolume(0.75f);
 }
 
 void ofApp::update() 
 {
 	cam.update();
+	ofSoundUpdate();
+	if (cam.isFrameNew()) {
 
+		//copy webcam pixels to rgb image
+		//rgb.setFromPixels(movie.getPixels(), w, h);
+		rgb.setFromPixels(cam.getPixels());
+
+		//store the three channels as grayscale images
+
+		rgb.convertToGrayscalePlanarImages(red, green, blue);
+
+		green += blue;   // adding green and blue channels then will subtract from red chanel to filter out only red
+		red -= green;
+		//filter image based on the hue value were looking for
+		for (int i = 0 ; i < 1280 * 960 ; i++) {
+			filtered.getPixels()[i] = ofInRange(red.getPixels()[i], 0, 40) ? 0 : 255;
+		}
+
+		filtered.flagImageChanged();
+		//run the contour finder on the filtered image to find blobs with a certain hue
+		contours.findContours(filtered, 50, 1280*960 / 2, 1, false);//輪廓
+	}
 }
 
 void ofApp::draw() 
 {
-	
-	ofSetColor(255, 255, 255);
-	cam.draw(0, 0);//把攝影機的畫面畫出來
-	//Map
-	ofSetColor(255, 255, 0);
-	ofDrawRectangle(100, 0, 50, 800);//X 100~150 Y 0~800 0
-	ofDrawRectangle(250, 160, 50, 800);//X 250~300 Y 160~960 1
-	ofDrawRectangle(400, 0, 50, 800);//X 400~450 Y 0~800 2
-	ofDrawRectangle(550, 160, 50, 800);//X 550~600 Y 160~960 3
-	ofDrawRectangle(700, 0, 50, 800);//X 700~750 Y 0~800 4
-	ofDrawRectangle(800,800,100,100);
-	ofDrawRectangle(900, 600, 380, 50);//X 900~1280 Y 600~650 0
-	ofDrawRectangle(750, 450, 380, 50);//X 750~1130 Y 450~500 1 
-	ofDrawRectangle(900, 300, 380, 50);//X 900~1280 Y 300~350 2
-	ofDrawRectangle(750, 150, 380, 50);//X 750~1130 Y 150~200 3
-	ofDrawRectangle(900, 0, 380, 50);//X 900~1280 Y 0~50 4
-	ofSetColor(255, 0, 0);
-	ofDrawRectangle(750, 0, 150, 50);//goal X 750~900 Y 0~50
-	//Map End
-	//抓取顏色
-	//抓取顏色 結束
-	//判斷有沒有碰到 邊邊 或到紅色了
-	//ofCircle(contours.blobs[i].centroid.x, contours.blobs[i].centroid.y, 5);
-	int centerOfCircleX = 0; 
-	int centerOfCircleY = 0;
-	if (IsBump(centerOfCircleX, centerOfCircleY))//碰撞了輸了
+	if (isBeginGame)
 	{
-		
-	
+		ofSetColor(255, 255, 255);
+		cam.draw(0, 0);//把攝影機的畫面畫出來
+		//Map
+		ofSetColor(255, 255, 0);
+		ofDrawRectangle(100, 0, 50, 800);//X 100~150 Y 0~800 0
+		ofDrawRectangle(250, 160, 50, 800);//X 250~300 Y 160~960 1
+		ofDrawRectangle(400, 0, 50, 800);//X 400~450 Y 0~800 2
+		ofDrawRectangle(550, 160, 50, 800);//X 550~600 Y 160~960 3
+		ofDrawRectangle(700, 0, 50, 800);//X 700~750 Y 0~800 4
+		ofDrawRectangle(800, 800, 100, 100);
+		ofDrawRectangle(900, 600, 380, 50);//X 900~1280 Y 600~650 0
+		ofDrawRectangle(750, 450, 380, 50);//X 750~1130 Y 450~500 1 
+		ofDrawRectangle(900, 300, 380, 50);//X 900~1280 Y 300~350 2
+		ofDrawRectangle(750, 150, 380, 50);//X 750~1130 Y 150~200 3
+		ofDrawRectangle(900, 0, 380, 50);//X 900~1280 Y 0~50 4
+		ofSetColor(255, 0, 0);
+		ofDrawRectangle(750, 0, 150, 50);//goal X 750~900 Y 0~50
+		//Map End
+		//抓取顏色
+		contours.draw(0, 0);
+		ofSetColor(255, 255, 0);
+		ofFill();
+		//draw red circles for found blobs
+		for (int i = 0; i < contours.nBlobs; i++) {
+			ofCircle(contours.blobs[i].centroid.x, contours.blobs[i].centroid.y, 5);
+			int centerOfCircleX = contours.blobs[i].centroid.x;
+			int centerOfCircleY = contours.blobs[i].centroid.y;
+			if (IsBump(centerOfCircleX, centerOfCircleY))//碰撞了輸了
+			{
+				cout << "Lose Game" << endl;
+				isBeginGame = false;
+				synth.play();
+
+			}
+			else if ((centerOfCircleX + 5 >= 750 && centerOfCircleX + 5 <= 900)
+				&& (centerOfCircleY >= 0 && centerOfCircleY <= 50)) //沒碰撞而且碰到紅色的 贏了
+			{
+				cout << "Win Game" << endl;
+			}
+		}
+		//抓取顏色 結束
 	}
-	else if ((centerOfCircleX + 5 >= 750 && centerOfCircleX + 5 <= 900)
-		&& (centerOfCircleY >= 0 && centerOfCircleY <= 50)) //沒碰撞而且碰到紅色的 贏了
+	else
 	{
+		ofSetColor(255, 255, 255);
+		cam.draw(0, 0);
+		ofSetColor(0, 255, 255);
+		ofDrawRectangle(440, 325, 400, 250);//X 550~600 Y 160~960 3
+	
 	
 	}
 
@@ -73,4 +119,11 @@ bool IsBump(int centerOfCircleX, int centerOfCircleY)
 		if ((centerOfCircleX >= rangeOfX && centerOfCircleX <= rangeOfX + 380) && (centerOfCircleY - 5 >= 600 - i * 150 && centerOfCircleY - 5 <= 650 - i * 150)) IsLoser = true;
 	}
 	return IsLoser;
+}
+void ofApp::keyPressed(int key) 
+{
+	if (key == 's')//Start Game
+	{
+		isBeginGame = true;
+	}
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,7 +2,7 @@
 
 #include "ofMain.h"
 #include "ofxCv.h"
-
+#include "ofxOpenCv.h"
 
 class ofApp : public ofBaseApp 
 {
@@ -11,9 +11,14 @@ public:
 	void setup();//程式一開始跑的
 	void update();//這個函式將會被重複且固定的呼叫
 	void draw();//每次Update()跑完就會跑
+	void keyPressed(int key);
 	
 	ofVideoGrabber cam;//為了存取攝影機的
 	ofPixels videoInverted;
 	ofTexture videoTexture;
-	ofxCv::ObjectFinder finder;
+	ofxCvColorImage rgb;                           //rgb object for storing color image data
+	ofxCvGrayscaleImage filtered, red, green, blue;    //objects for storing grayscale image data
+	ofxCvContourFinder contours;
+	ofSoundPlayer  synth;
+	bool isBeginGame = false;
 };
